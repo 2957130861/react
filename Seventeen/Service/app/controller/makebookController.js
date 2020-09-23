6846e4524f44885adbe65b42d26063d4170bddbe
2957130861 <2957130861@qq.com>
2020-09-23 16:57:43 +0800

    delete

--- Seventeen/Service/app/controller/makebookController.js
+++
@@ -1,9 +0,0 @@
-const Controller = require('egg').Controller;
-class MakeBookController extends Controller {
-    //根据用户id获得该用户所有的书
-	async getMyBook() {
-		let r=await this.ctx.service.makebookService.getMyBook();
-		this.ctx.response.body=r;
-	}
-};
-module.exports = MakeBookController;
--- Seventeen/client-show/src/Store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux';
-import { reducer } from './Reducer.js'
-
-let store = createStore(reducer);
-export default store;
